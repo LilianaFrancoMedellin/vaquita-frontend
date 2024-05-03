c1fc1e5a44e141f4a8d6a9e4e7c329c41d4b874a
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-05-02 19:06:28 -0500

    Adding default colors and fredoka font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ export default {
       fontFamily: {
         sans: ["Fredoka", "sans-serif"],
       },
+      colors: {
+        "vaki-green": "#66B04C",
+        "vaki-red": "#FF2530",
+        "vaki-black": "#3A3A3A",
+        "vaki-primary": "#36190D",
+        "vaki-secondary": "#FFA72F",
+      },
     },
   },
   plugins: [],
