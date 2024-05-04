c80c75e39438ad6def81e577a5b2f16226286656
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-05-04 17:24:32 -0500

    Adding login and not found pages

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div className="bg-slate-200 border rounded">
+      <p className="h-40 w-full flex items-center justify-center font-medium">Login Page!</p>
+    </div>
+  );
+};
+
+export default LoginPage;
