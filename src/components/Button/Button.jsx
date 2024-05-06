4e44c93f530ade063d8a34da86804db64fe82679
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-05-06 17:17:22 -0500

    Allowing button to be disabled

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ text, action, size }) => {
+const Button = ({ disabled, text, action, size }) => {
   return (
     <button
+      disabled={!!disabled}
       onClick={action}
       type="button"
       className={`bg-vaki-primary text-white rounded cursor-pointer ${
         size === 'md' ? 'px-3 py-2' : 'px-2 py-1'
-      }`}
+      } ${disabled ? 'cursor-not-allowed opacity-70' : ''}`}
     >
       {text}
     </button>
