e80554339c89f91b8cdb447c72f44744e9f10a8f
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-05-24 15:35:06 -0500

    Updating button with new variant and styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,26 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ className, disabled, text, action, size = 'md', type }) => {
+const Button = ({
+  className,
+  disabled,
+  text,
+  action,
+  size = 'md',
+  type = 'button',
+  variant = 'default',
+}) => {
   return (
     <button
       disabled={!!disabled}
       onClick={action}
       type={type}
-      className={`bg-vaki-primary text-white hover:opacity-90 rounded cursor-pointer ${
-        className ?? ''
-      } ${size === 'md' ? 'px-3 py-2' : 'px-2 py-1'} ${
-        disabled ? 'cursor-not-allowed opacity-50' : ''
-      }`}
+      className={`${
+        variant === 'default'
+          ? 'bg-vaki-primary text-white hover:opacity-90 focus:opacity-90'
+          : 'text-vaki-primary hover:opacity-80 hover:bg-vaki-primary hover:text-white focus:opacity-80 focus:bg-vaki-primary focus:text-white'
+      } border-2 border-vaki-primary rounded cursor-pointer ${className ?? ''} ${
+        size === 'md' ? 'px-3 py-2' : 'px-2 py-1'
+      } ${disabled ? 'cursor-not-allowed opacity-50' : ''}`}
     >
       {text}
     </button>
@@ -19,8 +29,12 @@ const Button = ({ className, disabled, text, action, size = 'md', type }) => {
 
 Button.propTypes = {
   action: PropTypes.func,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
   text: PropTypes.string,
   size: PropTypes.string,
+  type: PropTypes.string,
+  variant: PropTypes.string,
 };
 
 export default Button;
