11957686344881a376bd326e5738b583dcad7a61
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-05-24 12:08:34 -0500

    Adding xs breakpoint to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Fredoka", "sans-serif"],
+        sans: ['Fredoka', 'sans-serif'],
       },
       colors: {
-        "vaki-green": "#66B04C",
-        "vaki-red": "#FF2530",
-        "vaki-black": "#3A3A3A",
-        "vaki-primary": "#36190D",
-        "vaki-secondary": "#FFA72F",
+        'vaki-green': '#66B04C',
+        'vaki-red': '#FF2530',
+        'vaki-black': '#3A3A3A',
+        'vaki-primary': '#36190D',
+        'vaki-secondary': '#FFA72F',
+      },
+      screens: {
+        xs: '320px',
+        ...defaultTheme.screens,
       },
     },
   },
