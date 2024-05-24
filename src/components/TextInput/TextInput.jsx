b50662169152169400bed441678fc3c89de6de70
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-05-24 15:36:33 -0500

    Adding test input component

---
+++ src/components/TextInput/TextInput.jsx
@@ -0,0 +1,41 @@
+import userIcon from '../../assets/user.svg';
+import passwordIcon from '../../assets/password.svg';
+import PropTypes from 'prop-types';
+
+const availableIcons = {
+  user: userIcon,
+  password: passwordIcon,
+};
+
+const TextInput = ({ icon, ariaLabel, type, placeholder, name, onChange, value }) => {
+  const showIcon = !!availableIcons[icon];
+
+  return (
+    <div className="relative">
+      <input
+        aria-label={ariaLabel}
+        className={`w-full border border-vaki-black rounded p-1 ${showIcon ? 'pr-8' : ''}`}
+        type={type || 'text'}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      {showIcon && (
+        <img className="absolute top-[5px] right-[5px]" src={availableIcons[icon]} alt="icon" />
+      )}
+    </div>
+  );
+};
+
+TextInput.propTypes = {
+  ariaLabel: PropTypes.string,
+  icon: PropTypes.string,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.string,
+};
+
+export default TextInput;
