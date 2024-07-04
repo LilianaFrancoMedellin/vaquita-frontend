392c9e27a2456516134af5151f8c78d7cedb9ede
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-07-04 16:33:00 -0500

    Fixing disabled button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -9,18 +9,32 @@ const Button = ({
   type = 'button',
   variant = 'default',
 }) => {
+  const sizeClass = size === 'md' ? 'px-3 py-2' : 'px-2 py-1';
+  if (disabled) {
+    return (
+      <button
+        disabled
+        type={type}
+        className={`${
+          variant === 'default' ? 'bg-vaki-primary text-white' : 'text-vaki-primary'
+        } border-2 border-vaki-primary rounded ${
+          className ?? ''
+        } ${sizeClass} cursor-not-allowed opacity-50`}
+      >
+        {text}
+      </button>
+    );
+  }
+
   return (
     <button
-      disabled={!!disabled}
       onClick={action}
       type={type}
       className={`${
         variant === 'default'
           ? 'bg-vaki-primary text-white hover:opacity-90 focus:opacity-90'
           : 'text-vaki-primary hover:opacity-80 hover:bg-vaki-primary hover:text-white focus:opacity-80 focus:bg-vaki-primary focus:text-white'
-      } border-2 border-vaki-primary rounded cursor-pointer ${className ?? ''} ${
-        size === 'md' ? 'px-3 py-2' : 'px-2 py-1'
-      } ${disabled ? 'cursor-not-allowed opacity-50' : ''}`}
+      } border-2 border-vaki-primary rounded cursor-pointer ${className ?? ''} ${sizeClass}`}
     >
       {text}
     </button>
