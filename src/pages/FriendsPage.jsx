ed5cc36b82edd454050537c4d06f1922eb7086a8
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-07-05 16:18:34 -0500

    Implementing friends page with users

--- src/pages/FriendsPage.jsx
+++ src/pages/FriendsPage.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
+import * as userService from 'src/services/UserService';
+import Card from 'src/components/Card/Card';
+
 const FriendsPage = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    userService
+      .getAll()
+      .then((res) => {
+        setFriends(res.data.users);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div className="bg-slate-200 border rounded">
-      <p className="h-40 w-full flex items-center justify-center font-medium">
-        Friends Page Works!
-      </p>
+    <div className="flex flex-col">
+      <div className="flex gap-2 flex-wrap md:flex-none justify-center">
+        {friends.map((user, index) => (
+          <Card key={index} className="w-full sm:w-[calc(50%-4px)] xl:w-[calc(100%/3-8px)]">
+            <h2 className="text-xl">{user.name}</h2>
+            <span className="text-base">{user.email}</span>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
