f244a493962e100aa006b6b8c88ddf0a138e5a0c
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-07-07 18:29:52 -0500

    Implementing expenses list and addition

--- src/pages/GroupsDetailPage/GroupsDetailPage.jsx
+++ src/pages/GroupsDetailPage/GroupsDetailPage.jsx
@@ -3,43 +3,22 @@ import { useParams, useNavigate } from 'react-router-dom';
 import Button from 'src/components/Button/Button';
 import Card from 'src/components/Card/Card';
 import * as groupsService from 'src/services/GroupService';
+import * as expenseService from 'src/services/ExpenseService';
 import EditModal from './components/EditModal/EditModal';
 import AddFriendModal from './components/AddFriendModal/AddFriendModal';
 import DeleteModal from 'src/pages/GroupsDetailPage/components/DeleteModal/DeleteModal';
-
-const expenses = [
-  {
-    id: 1,
-    name: 'Café en Cali',
-    participants: 8,
-    paidBy: 'Juan Guillermo',
-    value: 25000,
-    owe: 0,
-  },
-  {
-    id: 2,
-    name: 'Cine - Poor things',
-    participants: 4,
-    paidBy: 'Alicia',
-    value: 105000,
-    owe: 45000,
-  },
-  {
-    id: 3,
-    name: 'Almuerzo de clase',
-    participants: 5,
-    paidBy: 'Liliana',
-    value: 25000,
-    owe: 0,
-  },
-];
+import AddExpenseModal from './components/AddExpenseModal/AddExpenseModal';
+import Message from 'src/components/Message/Message';
 
 const GroupsDetailPage = () => {
   const [group, setGroup] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isModalOpenAddFriend, setIsModalOpenAddFriend] = useState(false);
   const [isModalOpenDelete, setIsModalOpenDelete] = useState(false);
+  const [isModalOpenAddExpense, setIsModalOpenAddExpense] = useState(false);
+  const [expenses, setExpenses] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isLoadingExpenses, setIsLoadingExpenses] = useState(true);
 
   const navigate = useNavigate();
   const params = useParams();
@@ -57,12 +36,26 @@ const GroupsDetailPage = () => {
       });
   };
 
+  const fetchExpenses = () => {
+    expenseService
+      .getAllByGroup(params.id)
+      .then((res) => {
+        setExpenses(res.data.expenses);
+        setIsLoadingExpenses(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoadingExpenses(false);
+      });
+  };
+
   useEffect(() => {
     if (!params.id) {
       return;
     }
 
     fetchGroup();
+    fetchExpenses();
   }, [params]);
 
   if (!group && !isLoading) {
@@ -79,7 +72,7 @@ const GroupsDetailPage = () => {
     <>
       <div className="flex flex-col gap-8">
         <div className="flex flex-col xs:flex-row xs:justify-end my-4 gap-2 sm:gap-4">
-          <Button text="New Expense" action={() => console.log('click on new expense')} />
+          <Button text="New Expense" action={() => setIsModalOpenAddExpense(true)} />
           <Button text="New Friend" action={() => setIsModalOpenAddFriend(true)} />
           {imTheOwner && <Button text="Edit Group" action={() => setIsModalOpen(true)} />}
         </div>
@@ -110,10 +103,10 @@ const GroupsDetailPage = () => {
               key={index}
               className="w-full sm:w-[calc(50%-4px)] xl:w-[calc(100%/3-8px)]"
             >
-              <h2 className="text-xl">{expense.name}</h2>
+              <h2 className="text-xl">{expense.description}</h2>
               <span className="text-base">
                 <span className="text-vaki-secondary">{expense.paidBy} </span> paid{' '}
-                <span>${expense.value}</span>
+                <span>${expense.total}</span>
               </span>
               <span className="text-base">
                 {expense.owe > 0 ? `I owe $${expense.owe}` : 'I did not participate'}
@@ -124,6 +117,9 @@ const GroupsDetailPage = () => {
               </div>
             </Card>
           ))}
+          {!expenses.length && !isLoadingExpenses && (
+            <Message text="Looks like the group does not have Expenses yet" />
+          )}
         </div>
       </div>
       {isModalOpen && (
@@ -148,6 +144,14 @@ const GroupsDetailPage = () => {
           onSuccess={() => navigate('/groups')}
           isOpen={isModalOpenDelete}
           setIsOpen={setIsModalOpenDelete}
+        />
+      )}
+      {isModalOpenAddExpense && (
+        <AddExpenseModal
+          group={group}
+          onSuccess={fetchExpenses}
+          isOpen={isModalOpenAddExpense}
+          setIsOpen={setIsModalOpenAddExpense}
         />
       )}
     </>
